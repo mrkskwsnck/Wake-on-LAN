f121826287752473912407f6d89417390e14f5a0
Markus Kwaśnicki <markuskwasnicki@Mac-mini.local>
2011-12-17 12:49:07 +0100

    Initial Commit

---
+++ Wake on LAN/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Wake on LAN
+//
+//  Created by Markus Kwaśnicki on 17.12.11.
+//  Copyright (c) 2011 Kwasniccy. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
